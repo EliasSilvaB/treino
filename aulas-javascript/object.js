3aade5e518d074040bf67f94a660be900401f0d9
elias.silva36@hotmail.com <elias.silva36@hotmail.com>
2022-08-31 14:59:15 -0400

    git

---
+++ aulas-javascript/object.js
@@ -0,0 +1,49 @@
+let pessoa = {
+  nome:'elias',
+  idade:28,
+  estaraprovado:true,
+  pesoo:66,
+  braços:'fortes',
+  cafê:'gostoso'
+  
+  
+}
+console.log(pessoa)
+
+let nome1 = [1,2]
+console.log(nome1.length)
+console.log(nome1[1])
+
+let familia = [true,false,10,'dinheiro']
+console.log(familia.length)
+
+function tomate(){
+  let dindin= 1;
+  
+   if(dindin>=1){
+    console.log("vou comprar um");
+    
+  }
+  
+  else{
+    console.log('ta muito caro')
+  }
+  
+}
+tomate()
+let money = 12;
+console.log(money)
+function queroMoney(){
+  money = 10;
+}
+queroMoney()
+console.log(money)
+
+
+let cor = "vermelho"
+function mudarCor(preto,azul){
+  cor = preto+azul;
+}
+console.log(cor)
+mudarCor("tomato" ," azul escuro")
+console.log(cor)
---
+++ aulas-javascript/trocarValores.js
@@ -0,0 +1,47 @@
+let a = 'nada';
+if(a ==='vale dinheiro'){
+  console.log('muito money')
+  
+}
+else{
+  console.log('não vale nada')
+}
+let gato=5;
+
+switch(gato){
+  case 8:
+    console.log('tenho oito gatos')
+    break;
+    case 7:
+      console.log('tenho 7 gatos')
+      break;
+      case 6:
+        console.log('tenho 6 gatos')
+        break;
+        default:
+        console.log('tenho muitos gatos')
+}
+
+for(let i = 1; i<3;i++){
+  console.log(i)
+  
+}
+for(let oi =10;oi<12;oi++){
+  console.log('oi sumida')
+}
+
+let i =0;
+while(i <3){
+    console.log('digite' ,i)
+  i++;
+}
+
+
+let o =0
+do{
+  console.log('contando',o)
+  o++;
+}while(o<3)
+
+
+
